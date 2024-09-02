02318639762917563a21804b6c2c0a9cdf41b233
Leandr0Arantes <leandro.arantes0610@gmail.com>
2024-09-02 20:37:10 -0300

    Atualização de segurança

--- principal.php
+++ principal.php
@@ -1,8 +1,9 @@
 <?php
-session_start();
+include("valida.php");
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,19 +11,27 @@ session_start();
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
+
 <body>
     <header>
-        <i class="bi bi-file-person"></i>
-        <p><?php echo "Ola ".$_SESSION["nome"];?></p>
-        <button><a href="index.php">Sair</a></button>
+        <div class="user">
+            <i class="bi bi-file-person"></i>
+            <p><?php echo "Ola " . $_SESSION["nome"]; ?></p>
+        </div>
+        <button><a href="sair.php">Sair</a></button>
     </header>
-    <div class="menu">
-        <ul>
-            <li>item 1</li>
-            <li>item 2</li>
-            <li>item 3</li>
-        </ul>
+    <div style="padding: 2.75em;"></div>
+    <div class="main">
+        <div class="menu">
+            <ul>
+                <li>item 1</li>
+                <li>item 2</li>
+                <li>item 3</li>
+            </ul>
+        </div>
+        <div class="conteudo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod libero nihil, ea nisi nesciunt vitae adipisci corporis sunt optio ipsum, repudiandae dolorem porro accusantium inventore ab quia, minus delectus tempore!</div>
     </div>
-    <div class="conteudo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod libero nihil, ea nisi nesciunt vitae adipisci corporis sunt optio ipsum, repudiandae dolorem porro accusantium inventore ab quia, minus delectus tempore!</div>
+
 </body>
+
 </html>
