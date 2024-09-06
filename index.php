7539db9cabd8a716b823b87e97feb18bb58b98c2
Leandr0Arantes <leandro.arantes0610@gmail.com>
2024-09-06 20:10:52 -0300

    Formulário

--- index.php
+++ index.php
@@ -15,14 +15,14 @@
     </script>
     <link rel="stylesheet" href="index.css">
 </head>
-
 <body>
     <form method="post" action="login.php" onSubmit="return valida();" class="formulario">
-            <label for="cpf">CPF:</label>
-            <input type="text" name="cpf" id="cpf">
-            <label for="senha">Senha: </label>
-            <input type="text" name="senha" id="senha">
-            <input type="submit" value="Login">
+                <p>Login</p>
+                <label for="cpf">CPF:</label>
+                <input type="text" name="cpf" id="cpf">
+                <label for="senha">Senha: </label>
+                <input type="password" name="senha" id="senha">
+                <input type="submit" value="Login">
     </form>
 </body>
 
