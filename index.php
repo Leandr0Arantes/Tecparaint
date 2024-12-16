4b62e53aabb14ce363c1622708477feb784a3a38
Walysson Ribeiro Rosa <166772728+oCrazyoff@users.noreply.github.com>
2024-12-16 20:23:49 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include("funcoes/validacoes.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Entrar</title>
+    <title>SAIR - Walysson</title>
     <link rel="stylesheet" href="./css/index.css">
     <link rel="stylesheet" href="./css/form.css">
 </head>
